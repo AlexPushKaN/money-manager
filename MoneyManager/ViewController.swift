d7a39d9d50d44816f1d5b5c9a51d04a88c115d86
Александр Муклинов <dkan425@gmail.com>
2023-05-29 14:43:44 +0400

    Initial Commit

---
+++ MoneyManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyManager
+//
+//  Created by Александр Муклинов on 29.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
